bc1af990f13faabc3f450a73578c609b7c7b695d
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-05-09 01:01:25 +0600

    create API with Express Router

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -2,30 +2,34 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
+const router = express.Router();
 
 app.get('/', (req, res)=>{
     res.send('Hello from the Car API..!');
 });
 
-app.get('api/v1/cars', (req, res)=>{
+router.get('/', (req, res)=>{
     res.send('Here you can find all the cars..!');
 });
 
-app.post('api/v1/cars', (req, res)=>{
+router.post('/', (req, res)=>{
     res.send('Here you can create a new car..!');
 });
 
-app.put('api/v1/cars/:id', (req, res)=>{
+router.put('/:id', (req, res)=>{
     res.send('Here you can update an existing car..!');
 });
 
-app.delete('api/v1/cars/:id', (req, res)=>{
+router.delete('/:id', (req, res)=>{
     res.send('Here you can delete an existing car..!');
 });
 
-app.get('api/v1/cars/:id', (req, res)=>{
+router.get('/:id', (req, res)=>{
     res.send('Here you can find a specific car..!');
 });
+
+
+app.use('/api/v1', router);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on the port: ${PORT}`);

f1445fd25642854d5a83d6e5b6012b15e7b7ece0
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-05-09 00:54:00 +0600

    Create API with normal Express look like

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -4,7 +4,27 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('You just built a server with Express.js');
+    res.send('Hello from the Car API..!');
+});
+
+app.get('api/v1/cars', (req, res)=>{
+    res.send('Here you can find all the cars..!');
+});
+
+app.post('api/v1/cars', (req, res)=>{
+    res.send('Here you can create a new car..!');
+});
+
+app.put('api/v1/cars/:id', (req, res)=>{
+    res.send('Here you can update an existing car..!');
+});
+
+app.delete('api/v1/cars/:id', (req, res)=>{
+    res.send('Here you can delete an existing car..!');
+});
+
+app.get('api/v1/cars/:id', (req, res)=>{
+    res.send('Here you can find a specific car..!');
 });
 
 app.listen(PORT, ()=>{

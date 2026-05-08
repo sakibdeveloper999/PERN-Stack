b06fab8cb1c28f4c385f6934c23eb64cf3ac6daf
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-05-09 00:06:27 +0600

    Create a server with Node.js

---
+++ 01_nodejs/server.js
@@ -0,0 +1,9 @@
+import http from 'http';
+
+const server = http.createServer((req, res)=>{
+     res.writeHead(200, {'Content-Type': 'text/plain'});
+     res.end('You just a built a server with Node.js');
+})
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});

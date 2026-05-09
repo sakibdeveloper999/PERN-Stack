b5c0324c9f4ae8a9b9dd40802a46cf3a7a268c00
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-05-09 16:30:44 +0600

    Create a array and add some demo data that work as a database

--- 02_expressjs/server.js
+++ 02_expressjs/server.js
@@ -4,6 +4,14 @@ const app = express();
 const PORT = 3000;
 const router = express.Router();
 
+let cars = [
+    {id: 1, make: 'Toyota', model: 'Camry', year: 2020, color: 'Red', price: 25000},
+    {id: 2, make: 'Honda', model: 'Civic', year: 2021, color: 'Blue', price: 22000},
+    {id: 3, make: 'Ford', model: 'Mustang', year: 2019, color: 'Black', price: 30000},
+    {id: 4, make: 'Chevrolet', model: 'Impala', year: 2018, color: 'White', price: 20000},
+    {id: 5, make: 'Tesla', model: 'Model 3', year: 2022, color: 'Silver', price: 35000}
+];  
+
 app.get('/', (req, res)=>{
     res.send('Hello from the Car API..!');
 });
